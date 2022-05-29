622288812ed1c09cc327ac0215c3823f957119fd
Zueon <maineon0701@gmail.com>
2022-05-29 16:07:38 +0900

    connect mongoDB using mongoose

--- app.js
+++ app.js
@@ -1,26 +1,36 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const mongoose = require('mongoose');
 const app = express();
 
-const items = [];
 const workItems = [];
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
+
+mongoose.connect('mongodb://localhost:27017/todolistDB');
+
+const itemsSchema = {
+  name: String,
+};
+
+const Item = mongoose.model('Item', itemsSchema);
+const item1 = new Item({ name: 'todo list using mongoose!' });
+const item2 = new Item({ name: 'Hit the + button to add a new item' });
+const item3 = new Item({ name: 'Hit the checkbox to delete an item' });
+
+const defaultItems = [item1, item2, item3];
+Item.insertMany(defaultItems, (err) => {
+  if (err) {
+    console.log('Error');
+  } else {
+    console.log('Success');
+  }
+});
+
 app.get('/', (req, res) => {
-  const today = new Date();
-
-  const options = {
-    weekday: 'long',
-    day: 'numeric',
-    month: 'long',
-  };
-
-  let day = today.toLocaleDateString('en-US', options);
-
-  res.render('list', { listTitle: day, newItems: items });
+  res.render('list', { listTitle: 'Today', newItems: items });
 });
 
 app.post('/', (req, res) => {
